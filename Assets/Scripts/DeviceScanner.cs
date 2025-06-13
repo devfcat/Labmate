368d3f69dd2a0917fff6749b4680b4064f3850fa
Devfcat <137423690+devfcat@users.noreply.github.com>
2025-06-13 18:41:57 +0900

    [feat] 디바이스 연결 작업
    cursor가 작업함

--- Assets/Scripts/DeviceScanner.cs
+++ Assets/Scripts/DeviceScanner.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class DeviceScanner : MonoBehaviour
 {
@@ -14,6 +15,8 @@ public class DeviceScanner : MonoBehaviour
 
     private UdpClient udpClient;
     private int listenPort = 4210;
+    private Dictionary<string, bool> connectedDevices = new Dictionary<string, bool>();
+    private Dictionary<string, Button> deviceButtons = new Dictionary<string, Button>();
 
     private void Start()
     {
@@ -31,37 +34,83 @@ public class DeviceScanner : MonoBehaviour
 
             Debug.Log($"받은 장치: {deviceID} from {senderIP}");
 
-            AddDeviceToList(deviceID, senderIP);
+            if (!deviceButtons.ContainsKey(deviceID))
+            {
+                AddDeviceToList(deviceID, senderIP);
+            }
         }
     }
 
     private void AddDeviceToList(string id, string ip)
     {
-        // 중복 방지 로직 생략 가능
         GameObject btn = Instantiate(deviceButtonPrefab, contentHolder);
-        btn.GetComponentInChildren<Text>().text = id;
+        Button button = btn.GetComponent<Button>();
+        Text buttonText = btn.GetComponentInChildren<Text>();
+        buttonText.text = id;
 
-        btn.GetComponent<Button>().onClick.AddListener(() =>
+        deviceButtons[id] = button;
+        connectedDevices[id] = false;
+
+        button.onClick.AddListener(() =>
         {
-            Debug.Log($"장치 선택됨: {id} / {ip}");
-            // 여기에 연결 시도 로직 추가
+            if (!connectedDevices[id])
+            {
+                StartCoroutine(ConnectToDevice(ip, id));
+            }
+            else
+            {
+                StartCoroutine(DisconnectFromDevice(ip, id));
+            }
         });
     }
 
-    private void OnApplicationQuit()
-    {
-        udpClient?.Close();
-    }
-    
-    IEnumerator SendConnectRequest(string ip)
+    private IEnumerator ConnectToDevice(string ip, string deviceId)
     {
         UnityWebRequest www = UnityWebRequest.Get($"http://{ip}/connect");
         yield return www.SendWebRequest();
 
         if (www.result == UnityWebRequest.Result.Success)
-            Debug.Log("연결 성공!");
+        {
+            Debug.Log($"장치 연결 성공: {deviceId}");
+            connectedDevices[deviceId] = true;
+            UpdateButtonState(deviceId);
+        }
         else
-            Debug.Log("연결 실패");
+        {
+            Debug.Log($"장치 연결 실패: {deviceId}");
+        }
+    }
+
+    private IEnumerator DisconnectFromDevice(string ip, string deviceId)
+    {
+        UnityWebRequest www = UnityWebRequest.Get($"http://{ip}/disconnect");
+        yield return www.SendWebRequest();
+
+        if (www.result == UnityWebRequest.Result.Success)
+        {
+            Debug.Log($"장치 연결 해제: {deviceId}");
+            connectedDevices[deviceId] = false;
+            UpdateButtonState(deviceId);
+        }
+        else
+        {
+            Debug.Log($"장치 연결 해제 실패: {deviceId}");
+        }
+    }
+
+    private void UpdateButtonState(string deviceId)
+    {
+        if (deviceButtons.TryGetValue(deviceId, out Button button))
+        {
+            ColorBlock colors = button.colors;
+            colors.normalColor = connectedDevices[deviceId] ? Color.green : Color.white;
+            button.colors = colors;
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        udpClient?.Close();
     }
 }
 
