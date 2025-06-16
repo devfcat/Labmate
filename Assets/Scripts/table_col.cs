bbf4b808bf05668aa63bc7f1907b16088aaf08d6
Devfcat <137423690+devfcat@users.noreply.github.com>
2025-06-17 07:11:44 +0900

    [feat] 테이블 스크립트

---
+++ Assets/Scripts/table_col.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class table_col : MonoBehaviour
+{
+    public TextMeshProUGUI tmp_index;
+    public TextMeshProUGUI tmp_value;
+
+    
+}
